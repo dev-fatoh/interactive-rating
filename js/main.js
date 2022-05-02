139df17fbe29f7c681dbf349abee13b5825a31ff
dev-fatoh <fatohnasr360@gmail.com>
2022-05-02 08:02:24 +0200

    add js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container')
+
+const thanks = document.querySelector('.thanks')
+console.log(thanks)
+const button = document.querySelector(".btn")
+const ratList = document.querySelectorAll('.rate')
+const msg = document.getElementById('msg')
+
+button.addEventListener('click', ()=> {
+    container.classList.add('hide')
+    thanks.classList.add('show')
+})
+
+ratList.forEach(item=> {
+    item.addEventListener('click', function() {
+        msg.textContent = `You selected ${item.innerText} out of 5`
+    })
+})
